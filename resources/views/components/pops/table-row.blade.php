7797d6bedaf901cfc080087decb6f8c5b0d8dd23
Okidokie98 <baat0004@hz.nl>
2025-01-02 13:41:22 +0100

    Number to the front + removed info-button

--- resources/views/components/pops/table-row.blade.php
+++ resources/views/components/pops/table-row.blade.php
@@ -1,7 +1,7 @@
 <tr {{ $attributes->merge(['class' => '']) }} class="justify-center">
-    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$pop->name}}</td>
-    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$pop->series}}</td>
     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$pop->number}}</td>
+    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><a href="{{ route('pops.show', $pop) }}">{{$pop->name}}</a></td>
+    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$pop->series}}</td>
     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$pop->category}}</td>
-    <td> <a href="{{ route('pops.show', $pop) }}" class="inline-block m-2 p-2 text-white bg-blue-500 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-md">Info</a></td>
+
 </tr>
--- resources/views/pops/index.blade.php
+++ resources/views/pops/index.blade.php
@@ -54,9 +54,9 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead>
                     <tr>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Naam</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Series</th>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nummer</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Categorië</th>
                     </tr>
                     </thead>
